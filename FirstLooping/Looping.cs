9a82f6e280a64c18d2020362b7f601f90b9cb881
Tresna Nurziyan <tresna.nurziyan@gmail.com>
2017-07-26 11:43:57 +0700

    First looping in c sharp

---
+++ FirstLooping/Looping.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FirstLooping
+{
+    class Looping
+    {
+        static void Main(String[] args)
+        {
+            for (int a = 0, b = 5; ((a < 5) && (b > 0)); b--, a++)
+            {
+                Console.WriteLine("{0} {1} {2}\t", a, (a > b ? '>' : '<'), b);
+                //Console.WriteLine(b == 0 ? "Perbandingan selesai" : "Perbandingan selanjutnya");
+
+                if(a == 4)
+                {
+                    b--;
+                    Console.WriteLine("Perbandingan Selesai");
+                }
+                else
+                {
+                    Console.WriteLine("Perbandingan selanjutnya");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
